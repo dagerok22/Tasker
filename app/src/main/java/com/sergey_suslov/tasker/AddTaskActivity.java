297276a4757527aba0c3e82648d34406a9ab4950
dagerok22 <dagerok22@gmail.com>
2017-03-28 16:00:41 +0700

    somthing added

--- app/src/main/java/com/sergey_suslov/tasker/AddTaskActivity.java
+++ app/src/main/java/com/sergey_suslov/tasker/AddTaskActivity.java
@@ -72,7 +72,7 @@ public class AddTaskActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        setTheme(R.style.AppThemeColorBlue);
+        setTheme(R.style.AppTheme);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_new_task);
         mIsUrgent = false;
